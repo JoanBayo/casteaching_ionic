3355f8bd336b5e4c469ead646f7b8b21e0ccbc96
Bayo <jbayo@iesebre.com>
2023-03-09 17:18:26 +0100

    Serie Ionic Casteaching, Final video 3 i 4

--- src/views/Video.vue
+++ src/views/Video.vue
@@ -5,65 +5,85 @@
         <ion-buttons slot="start">
           <ion-menu-button color="primary"></ion-menu-button>
         </ion-buttons>
-        <ion-title>Video {{ $route.params.id }}</ion-title>
+        <ion-title>{{ video.title }}</ion-title>
       </ion-toolbar>
     </ion-header>
-    
+
     <ion-content :fullscreen="true">
       <ion-header collapse="condense">
         <ion-toolbar>
           <ion-title size="large">Video {{ $route.params.id }}</ion-title>
         </ion-toolbar>
       </ion-header>
-    
+
       <div id="container">
-        <strong class="capitalize">Video {{ $route.params.id }}</strong>
-        <p>TODO VIDEO</p>
+        <iframe id="video" :src="video.url" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+        <ion-card>
+          <ion-card-header>
+            <ion-card-title>{{ video.title }}</ion-card-title>
+            <ion-card-subtitle>{{ video.published_at }}</ion-card-subtitle>
+          </ion-card-header>
+
+          <ion-card-content>
+            {{ video.description }}
+          </ion-card-content>
+        </ion-card>
       </div>
     </ion-content>
   </ion-page>
 </template>
 
 <script>
-import { IonButtons, IonContent, IonHeader, IonMenuButton, IonPage, IonTitle, IonToolbar } from '@ionic/vue';
+
+import casteaching from '@acacha/casteaching'
+const api = casteaching({baseUrl: 'http://casteachingbayo.test/api/'})
+
+import {
+  IonButtons,
+  IonCardHeader, IonCardSubtitle, IonCardTitle,
+  IonCard,
+  IonContent,
+  IonCardContent,
+  IonHeader,
+  IonMenuButton,
+  IonPage,
+  IonTitle,
+  IonToolbar
+} from '@ionic/vue';
 
 export default {
-  name: 'FolderBayo',
+  name: 'VideoBayo',
   components: {
     IonButtons,
     IonContent,
+    IonCard,
     IonHeader,
     IonMenuButton,
     IonPage,
     IonTitle,
-    IonToolbar
+    IonToolbar,
+    IonCardHeader,
+    IonCardTitle,
+    IonCardSubtitle,
+    IonCardContent
+  },
+  data() {
+    return {
+      video: {}
+    }
+  },
+
+  async created() {
+    this.video = await api.video.show(this.$route.params.id)
   }
 }
 </script>
 
 <style scoped>
-#container {
-  text-align: center;
-  position: absolute;
-  left: 0;
-  right: 0;
-  top: 50%;
-  transform: translateY(-50%);
+
+#video {
+  width:100%;
+  height:70vh;
 }
 
-#container strong {
-  font-size: 20px;
-  line-height: 26px;
-}
-
-#container p {
-  font-size: 16px;
-  line-height: 22px;
-  color: #8c8c8c;
-  margin: 0;
-}
-
-#container a {
-  text-decoration: none;
-}
 </style>
